a08d5613feec59e3661f257cd498de0438c2b8b0
Vlad <00cmb.ua@gmail.com>
2023-08-13 14:55:49 +0300

    fix

--- src/js/main.js
+++ src/js/main.js
@@ -9,69 +9,84 @@ const elements = {
   loadMore: document.querySelector(`.load-more`),
 };
 
-const gallery = new SimpleLightbox('.gallery a');
+let gallery = null;
 let page = 1;
 let currentSearch;
-let totalHits;
-
+let totalHits = 0;
+let totalPages = totalHits / 40;
 elements.searchForm.addEventListener(`submit`, newSearch);
 elements.loadMore.addEventListener(`click`, loadMoreImg);
 
 async function createItems() {
-  let galleryMarkup;
-
   await getImages(currentSearch, page)
     .then(response => {
-      if (response.totalHits === 0) {
-        throw error;
+      if (!response.hits.length) {
+        Notiflix.Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+
+        elements.searchForm.reset();
+
+        return;
       }
       const galleryArr = response.hits;
       totalHits = response.totalHits;
-
-      galleryMarkup = galleryArr
-        .map(
-          hit => `<li class="gallery-item">
+      totalPages = totalHits / 40;
+      elements.gallery.insertAdjacentHTML(
+        `beforeend`,
+        galleryArr
+          .map(
+            hit => `<div class="gallery-item">
         <a class="gallery-link" href="${hit.largeImageURL}">
-  <img class ="gallery-image" src="${hit.webformatURL}" alt="${hit.tags}" height="240px" loading="lazy" /></a>
-  <ul class="info-box">
-    <li class="info-item">
+  <img class ="gallery-image" src="${hit.webformatURL}" alt="${hit.tags}" loading="lazy" /></a>
+  <div class="info-box">
       <p class="info-text">&#x1F44D ${hit.likes}</p>
-    </li>
-    <li class="info-text">
       <p class="info-views">&#x1F9D0 ${hit.views}</p>
-    </li>
-    <li class="info-text">
       <p class="info-text">&#x1F4AC ${hit.comments}</p>
-    </li>
-    <li class="info-item">
       <p class="info-text">&#x1F4E5 ${hit.downloads}</p>
-    </li>
-  </ul>
-</li>`
-        )
-        .join(``);
-      elements.gallery.insertAdjacentHTML(`beforeend`, galleryMarkup);
-      elements.loadMore.classList.remove(`isHidden`);
+  </div>
+</div>`
+          )
+          .join(``)
+      );
+
+      gallery.refresh();
+
+      const { height: cardHeight } =
+        elements.gallery.firstElementChild.getBoundingClientRect();
+
+      window.scrollBy({
+        top: cardHeight * 2,
+        behavior: 'smooth',
+      });
+
+      if (totalPages > page) {
+        elements.loadMore.classList.remove(`isHidden`);
+      }
     })
-    .catch(error => {
-      Notiflix.Notify.failure(
-        `Sorry, there are no images matching your search query. Please try again.`
+    .catch(({ code, message }) => {
+      Notiflix.Report.failure(
+        `${message}. Code: ${code} `,
+        'Oops! Something went wrong! Try reloading the page!',
+        'OK'
       );
     });
 }
-
 async function newSearch(e) {
   e.preventDefault();
 
   page = 1;
   currentSearch = e.currentTarget.firstElementChild.value;
-  elements.gallery.innerHTML = '';
+  elements.gallery.innerHTML = ``;
   elements.loadMore.classList.add(`isHidden`);
 
-  createItems();
-  smoothScroll();
+  await createItems();
+  if (totalHits <= 0) {
+    return;
+  }
 
-  gallery.refresh();
+  gallery = new SimpleLightbox('.gallery-link');
+
   Notiflix.Notify.success(`"Hooray! We found ${totalHits} images."`);
 
   page += 1;
@@ -79,24 +94,14 @@ async function newSearch(e) {
 }
 
 async function loadMoreImg() {
-  createItems();
-  smoothScroll();
+  await createItems();
+
   gallery.refresh();
 
   page += 1;
-  const totalPages = totalHits / 40;
+
   if (page > totalPages) {
     elements.loadMore.classList.add(`isHidden`);
     Notiflix.Notify.success(`There all images for your request. Try another`);
   }
 }
-
-// async function smoothScroll() {
-//   const { height: cardHeight } =
-//     elements.gallery.firstElementChild.getBoundingClientRect();
-
-//   window.scrollBy({
-//     top: cardHeight * 2.28,
-//     behavior: 'smooth',
-//   });
-// }
